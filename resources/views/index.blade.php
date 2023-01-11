6fea4d9166bebe396242cc580186db9f36f9fda7
Abdalrhman Khashashneh <abdalrhmankhashashneh@gmail.com>
2023-01-11 23:21:12 +0300

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     
 <section>
-    <h1>hi mister abd </h1>
+    <h1>hi mister abd hi baasil </h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem hic quas facere fuga ab temporibus, recusandae, similique laborum reiciendis iste a, ducimus fugit dicta repudiandae. Facilis eius ratione optio reprehenderit.
+    </p>
 </section>
 
 
